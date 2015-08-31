f701fc509b9a628f33bd4e7af6d78e41b05e8446
MZFischer <maiquel.fischer@ag2nurun.com.br>
2015-08-31 18:48:14 -0300

    Finalizada aula 33

--- Quiron.LojaVirtual.Dominio/Entidades/Produto.cs
+++ Quiron.LojaVirtual.Dominio/Entidades/Produto.cs
@@ -3,15 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace Quiron.LojaVirtual.Dominio.Entidades
 {
     public class Produto
     {
+        [HiddenInput(DisplayValue = false)]
         public int ProdutoId { get; set; }
+
+        [Required(ErrorMessage = "Digite o nome do Produto")]
         public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Digite a descrição")]
+        [DataType(DataType.MultilineText)]
         public string Descricao { get; set; }
+
+        [Required(ErrorMessage = "Digite o Preço do Produto")]
+        [Range(0.01, double.MaxValue, ErrorMessage ="Valor inválido")]
         public decimal Preco{ get; set; }
+
+        [Required(ErrorMessage = "Digite a Categoria")]
         public string Categoria { get; set; }
     }
 }
